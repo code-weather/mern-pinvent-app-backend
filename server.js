d37973f042d21c11a2d1cef607ffa221357e85a9
Jameson Wang <jamesonw10@gmail.com>
2023-02-02 11:37:08 -0500

    created custom error handler

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const userRoute = require("./routes/userRoutes")
+const userRoute = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorMiddleware');
 
 const app = express();
 
 // Middlewares
 app.use(express.json()); // Help handle JSON data in our application
-app.use(express.urlencoded({extended: false})) // Help handle data that comes via the URL
+app.use(express.urlencoded({ extended: false })); // Help handle data that comes via the URL
 
 // Routes Middleware
-app.use("/api/users", userRoute)
+app.use('/api/users', userRoute);
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Home page');
 });
+
+// Error Middleware
+app.use(errorHandler);
 
 // Connect to mongoDB and start server
 mongoose.set('strictQuery', true); // When strict option is set to true, Mongoose will ensure that only the fields that are specified in your Schema will be saved in the database, and all other fields will not be saved (if some other fields are sent).
