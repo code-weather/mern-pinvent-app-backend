9bc80ac3bd05966b09c3333eba8554469de0a4c9
Jameson Wang <jamesonw10@gmail.com>
2023-03-01 14:48:53 -0500

    get single product - route and controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -52,11 +52,27 @@ const createProduct = asyncHandler(async (req, res) => {
 
 // Get all Products
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({user: req.user.id}).sort('-createdAt')
-  res.status(200).json(products)
+  const products = await Product.find({ user: req.user.id }).sort('-createdAt');
+  res.status(200).json(products);
+});
+
+// Get single product
+const getSingleProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+  res.status(200).json(product);
 });
 
 module.exports = {
   createProduct,
   getProducts,
+  getSingleProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/authMiddleware');
-const { createProduct, getProducts } = require('../controllers/productController');
+const {
+  createProduct,
+  getProducts,
+  getSingleProduct,
+} = require('../controllers/productController');
 const { upload } = require('../utils/fileUpload');
 
 router.post('/', protect, upload.single('image'), createProduct); // upload.array('image') - if you want to upload multiple files
 router.get('/', protect, getProducts);
+router.get('/:id', protect, getSingleProduct);
 
-module.exports = router
+module.exports = router;
