1d557c50bd8ea4b9898eb506821edd3a451f9a06
Jameson Wang <jamesonw10@gmail.com>
2023-03-01 16:59:34 -0500

    created an update product route and controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -90,9 +90,70 @@ const deleteProduct = asyncHandler(async (req, res) => {
   res.status(200).json(product);
 });
 
+// Update product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, category, quantity, price, description } = req.body;
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  // If product doesn't exist
+  if (!product) {
+    res.status(404);
+    throw new Error('Product not  found');
+  }
+  // Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'Pinvent App',
+        resource_type: 'image',
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error('Image could not be uploaded');
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+
+  // Update Product
+  const updatedProduct = await Product.findByIdAndUpdate(
+    { _id: id },
+    {
+      name,
+      category,
+      quantity,
+      price,
+      description,
+      image: fileData || product.image,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json(updatedProduct);
+});
+
 module.exports = {
   createProduct,
   getProducts,
   getSingleProduct,
   deleteProduct,
+  updateProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,10 +6,12 @@ const {
   getProducts,
   getSingleProduct,
   deleteProduct,
+  updateProduct,
 } = require('../controllers/productController');
 const { upload } = require('../utils/fileUpload');
 
 router.post('/', protect, upload.single('image'), createProduct); // upload.array('image') - if you want to upload multiple files
+router.patch('/:id', protect, upload.single('image'), updateProduct);
 router.get('/', protect, getProducts);
 router.get('/:id', protect, getSingleProduct);
 router.delete('/:id', protect, deleteProduct);
