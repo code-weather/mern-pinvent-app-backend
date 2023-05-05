2417a893ceab4dcdcb92bb2da27fecdb525fdfdf
Jameson Wang <jamesonw10@gmail.com>
2023-05-05 15:05:26 -0400

    add cors

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ const path = require('path');
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://mern-pinvent-app-frontend-7cnu1rdal-code-weather.vercel.app'
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://mern-pinvent-app-frontend-7cnu1rdal-code-weather.vercel.app',
+      'https://pinvent-technologies.vercel.app',
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json()); // Help handle JSON data in our application
 app.use(express.urlencoded({ extended: false })); // Help handle data that comes via the URL
 app.use(cookieParser());
